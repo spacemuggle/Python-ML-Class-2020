02aaacabf1b4be5c16db0aae14fc32a5444fe838
spacemuggle <64231322+spacemuggle@users.noreply.github.com>
2020-05-10 13:43:11 -0400

    Added HWs and PCC folder

---
+++ HWs/HW4_test_import.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def tell_age(age):
+    """This is where you should describe what the function does"""
+    print('You are ' + str(age) + ' years old.')
+
